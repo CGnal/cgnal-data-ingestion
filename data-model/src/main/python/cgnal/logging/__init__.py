2326c63c044a56787ee3bae30f00578ff42d6ea8
Enrico <edeusebio@cgnal.com>
2018-07-17 15:32:28 +0200

    [MISC] Adding Logging Funcionalities

---
+++ data-model/src/main/python/cgnal/logging/__init__.py
@@ -0,0 +1,17 @@
+from abc import ABCMeta, abstractproperty
+
+DEFAULT_LOG_LEVEL="INFO"
+
+class WithLoggingABC(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractproperty
+    def logger(self):
+        """
+        Logging class to be used to output logs within a class
+        :return: None, outputs logs
+        """
+        pass
+
+
---
+++ data-model/src/main/python/cgnal/logging/simple.py
@@ -0,0 +1,76 @@
+from datetime import datetime as dt
+
+from . import WithLoggingABC
+from . import DEFAULT_LOG_LEVEL as LOG_LEVEL
+
+levels = {"ERROR": 0, "WARN": 1, "INFO": 2, "DEBUG": 3}
+
+class Logger(object):
+    """
+    A class to report different levels of log messages
+    """
+    def __init__(self, log_level):
+        self.log_level = log_level
+
+    def __printer__(self, msg, level):
+        """
+        :param msg: the output message to report
+        :param level: the level of the message
+        :type msg: str
+        :type level: str
+        :return:the message
+        """
+        if levels[self.log_level] >= levels[level]:
+            print("%s - %s: %s" % (str(dt.utcnow()), level, msg) )
+
+    def info(self, msg):
+        """
+        :param msg: the output message to report
+        :type msg: str
+        """
+        self.__printer__(msg, "INFO")
+
+    def debug(self, msg):
+        """
+        :param msg: the output message to report
+        :type msg: str
+        """
+        self.__printer__(msg, "DEBUG")
+
+    def warn(self, msg):
+        """
+        :param msg: the output message to report
+        :type msg: str
+        """
+        self.__printer__(msg, "WARN")
+
+    def error(self, msg):
+        """
+        :param msg: the output message to report
+        :type msg: str
+        """
+        self.__printer__(msg, "ERROR")
+
+
+_logger = Logger(LOG_LEVEL)
+
+
+def logger():
+    """
+    Initialize default logger
+
+    :return: default logger
+    """
+    return _logger
+
+
+class WithLogging(WithLoggingABC):
+
+    @property
+    def logger(self):
+        """
+        Initialize default logger
+
+        :return: default logger
+        """
+        return logger
