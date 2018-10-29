a5b471d4a98fb105b74409ecf96642feff03f81a
NicolaDonelli <ndonelli@cgnal.com>
2018-10-29 15:22:54 +0100

    [MISC] Updated resources folder structure

--- data-model/src/main/python/cgnal/tests/__init__.py
+++ data-model/src/main/python/cgnal/tests/__init__.py
@@ -1,5 +1,5 @@
 import os
 from cgnal.utils.fs import create_dir_if_not_exists
 
-DATA_FOLDER = create_dir_if_not_exists( os.path.join(os.getcwd(), "cgnal", "resources") )
-TMP_FOLDER = create_dir_if_not_exists( os.path.join(os.getcwd(), "cgnal", "resources", "tmp") )
+DATA_FOLDER = create_dir_if_not_exists(os.path.join(os.getcwd(), "cgnal", "resources", "tests", "data"))
+TMP_FOLDER = create_dir_if_not_exists(os.path.join(os.getcwd(), "cgnal", "resources", "tests", "tmp"))
--- data-model/src/main/python/cgnal/tests/model_ml.py
+++ data-model/src/main/python/cgnal/tests/model_ml.py
@@ -1,62 +1,151 @@
 import numpy as np
-from itertools import islice
+import unittest
 from cgnal.data.model.ml import LazyDataset, IterGenerator, MultiFeatureSample, Sample
 
+class LazyDatasetTests(unittest.TestCase):
 
-samples = [MultiFeatureSample(features=[np.array([100, 101]), np.array([np.NaN])], label=1),
-           MultiFeatureSample(features=[np.array([102, 103]), np.array([1])], label=2),
-           MultiFeatureSample(features=[np.array([104, 105]), np.array([2])], label=3),
-           MultiFeatureSample(features=[np.array([106, 107]), np.array([3])], label=4),
-           MultiFeatureSample(features=[np.array([108, 109]), np.array([4])], label=5),
-           MultiFeatureSample(features=[np.array([110, 111]), np.array([5])], label=6),
-           MultiFeatureSample(features=[np.array([112, 113]), np.array([6])], label=7),
-           MultiFeatureSample(features=[np.array([114, 115]), np.array([7])], label=8),
-           MultiFeatureSample(features=[np.array([116, 117]), np.array([8])], label=9)]
+    def test_withLookback_MultiFeatureSample(self):
+        samples = [MultiFeatureSample(features=[np.array([100., 101.]), np.array([np.NaN])], label=1.),
+                   MultiFeatureSample(features=[np.array([102., 103.]), np.array([1.])], label=2.),
+                   MultiFeatureSample(features=[np.array([104., 105.]), np.array([2.])], label=3.),
+                   MultiFeatureSample(features=[np.array([106., 107.]), np.array([3.])], label=4.),
+                   MultiFeatureSample(features=[np.array([108., 109.]), np.array([4.])], label=5.),
+                   MultiFeatureSample(features=[np.array([110., 111.]), np.array([5.])], label=6.),
+                   MultiFeatureSample(features=[np.array([112., 113.]), np.array([6.])], label=7.),
+                   MultiFeatureSample(features=[np.array([114., 115.]), np.array([7.])], label=8.),
+                   MultiFeatureSample(features=[np.array([116., 117.]), np.array([8.])], label=9.)]
+
+        def samples_gen():
+            for sample in samples:
+                if not any([np.isnan(x).any() for x in sample.features]):
+                    yield sample
+
+        X1 = np.array([[[102., 103.],[104., 105.],[106., 107.]], [[104., 105.],[106., 107.],[108., 109.]],
+                       [[106., 107.],[108., 109.],[110., 111.]], [[108., 109.],[110., 111.],[112., 113.]]])
+        y1 = np.array([[[1.],[2.],[3.]], [[2.],[3.],[4.]], [[3.],[4.],[5.]], [[4.],[5.],[6.]]])
+        lab1 = np.array([4., 5., 6., 7.])
+        X2 = np.array([[[110., 111.],[112., 113.],[114., 115.]], [[112., 113.],[114., 115.],[116., 117.]]])
+        y2 = np.array([[[5.],[6.],[7.]], [[6.],[7.],[8.]]])
+        lab2 = np.array([8., 9.])
+
+        lookback = 3
+        batch_size = 4
+
+        lazyDat = LazyDataset(IterGenerator(samples_gen))
+        lookbackDat = lazyDat.withLookback(lookback)
+        batch_gen = lookbackDat.batch(batch_size)
+
+        batch1=next(batch_gen)
+        batch2=next(batch_gen)
+
+        tmp1 = batch1.getFeaturesAs("array")
+        temp1X = np.array(list(map(lambda x: np.stack(x), tmp1[:, :, 0])))
+        temp1y = np.array(list(map(lambda x: np.stack(x), tmp1[:, :, 1])))
+        tmp1lab = batch1.getLabelsAs("array")
+
+        res = [np.array_equal(temp1X, X1), np.array_equal(temp1y, y1), np.array_equal(tmp1lab, lab1)]
+
+        tmp2 = batch2.getFeaturesAs("array")
+        temp2X = np.array(list(map(lambda x: np.stack(x), tmp2[:, :, 0])))
+        temp2y = np.array(list(map(lambda x: np.stack(x), tmp2[:, :, 1])))
+        tmp2lab = batch2.getLabelsAs("array")
+
+        res = res + [np.array_equal(temp2X, X2), np.array_equal(temp2y, y2), np.array_equal(tmp2lab, lab2)]
+        
+        self.assertTrue(all(res))
+
+    def test_withLookback_ArrayFeatureSample(self):
+        
+        samples = [Sample(features=np.array([100, 101]), label=1),
+                   Sample(features=np.array([102, 103]), label=2),
+                   Sample(features=np.array([104, 105]), label=3),
+                   Sample(features=np.array([106, 107]), label=4),
+                   Sample(features=np.array([108, 109]), label=5),
+                   Sample(features=np.array([110, 111]), label=6),
+                   Sample(features=np.array([112, 113]), label=7),
+                   Sample(features=np.array([114, 115]), label=8),
+                   Sample(features=np.array([116, 117]), label=9)]
+        
+        def samples_gen():
+            for sample in samples:
+                if not any([np.isnan(x).any() for x in sample.features]):
+                    yield sample
+
+        X1 = np.array([[[100, 101],[102, 103],[104, 105]], [[102, 103],[104, 105],[106, 107]],
+                       [[104, 105],[106, 107],[108, 109]], [[106, 107],[108, 109],[110, 111]]])
+        lab1 = np.array([3, 4, 5, 6])
+        X2 = np.array([[[108, 109],[110, 111],[112, 113]], [[110, 111], [112, 113],[114, 115]],
+                       [[112, 113],[114, 115],[116, 117]]])
+        lab2 = np.array([7, 8, 9])
+
+        lookback = 3
+        batch_size = 4
+
+        lazyDat = LazyDataset(IterGenerator(samples_gen))
+        lookbackDat = lazyDat.withLookback(lookback)
+        batch_gen = lookbackDat.batch(batch_size)
+
+        batch1 = next(batch_gen)
+        batch2 = next(batch_gen)
+
+        tmp1 = batch1.getFeaturesAs("array")
+        tmp1lab = batch1.getLabelsAs("array")
+
+        res = [np.array_equal(tmp1, X1), np.array_equal(tmp1lab, lab1)]
+
+        tmp2 = batch2.getFeaturesAs("array")
+        tmp2lab = batch2.getLabelsAs("array")
+
+        res = res + [np.array_equal(tmp2, X2), np.array_equal(tmp2lab, lab2)]
+
+        self.assertTrue(all(res))
+
+    def test_withLookback_ListFeatureSample(self):
+
+        samples = [Sample(features=[100, 101], label=1),
+                   Sample(features=[102, 103], label=2),
+                   Sample(features=[104, 105], label=3),
+                   Sample(features=[106, 107], label=4),
+                   Sample(features=[108, 109], label=5),
+                   Sample(features=[110, 111], label=6),
+                   Sample(features=[112, 113], label=7),
+                   Sample(features=[114, 115], label=8),
+                   Sample(features=[116, 117], label=9)]
+
+        def samples_gen():
+            for sample in samples:
+                if not any([np.isnan(x).any() for x in sample.features]):
+                    yield sample
+
+        X1 = np.array([[[100, 101], [102, 103], [104, 105]], [[102, 103], [104, 105], [106, 107]],
+                       [[104, 105], [106, 107], [108, 109]], [[106, 107], [108, 109], [110, 111]]])
+        lab1 = np.array([3, 4, 5, 6])
+        X2 = np.array([[[108, 109], [110, 111], [112, 113]], [[110, 111], [112, 113], [114, 115]],
+                       [[112, 113], [114, 115], [116, 117]]])
+        lab2 = np.array([7, 8, 9])
+
+        lookback = 3
+        batch_size = 4
+
+        lazyDat = LazyDataset(IterGenerator(samples_gen))
+        lookbackDat = lazyDat.withLookback(lookback)
+        batch_gen = lookbackDat.batch(batch_size)
+
+        batch1 = next(batch_gen)
+        batch2 = next(batch_gen)
+
+        tmp1 = batch1.getFeaturesAs("array")
+        tmp1lab = batch1.getLabelsAs("array")
+
+        res = [np.array_equal(tmp1, X1), np.array_equal(tmp1lab, lab1)]
+
+        tmp2 = batch2.getFeaturesAs("array")
+        tmp2lab = batch2.getLabelsAs("array")
+
+        res = res + [np.array_equal(tmp2, X2), np.array_equal(tmp2lab, lab2)]
+
+        self.assertTrue(all(res))
 
 
-# samples = [Sample(features=np.array([100, 101]), label=1),
-#            Sample(features=np.array([102, 103]), label=2),
-#            Sample(features=np.array([104, 105]), label=3),
-#            Sample(features=np.array([106, 107]), label=4),
-#            Sample(features=np.array([108, 109]), label=5),
-#            Sample(features=np.array([110, 111]), label=6),
-#            Sample(features=np.array([112, 113]), label=7),
-#            Sample(features=np.array([114, 115]), label=8),
-#            Sample(features=np.array([116, 117]), label=9)]
-#
-#
-# samples = [Sample(features=[100, 101], label=1),
-#            Sample(features=[102, 103], label=2),
-#            Sample(features=[104, 105], label=3),
-#            Sample(features=[106, 107], label=4),
-#            Sample(features=[108, 109], label=5),
-#            Sample(features=[110, 111], label=6),
-#            Sample(features=[112, 113], label=7),
-#            Sample(features=[114, 115], label=8),
-#            Sample(features=[116, 117], label=9)]
-
-
-def samples_gen():
-    for sample in samples:
-        if not any([np.isnan(x).any() for x in sample.features]):
-            yield sample
-
-
-lookback = 3
-batch_size = 4
-
-
-lazyDat = LazyDataset(IterGenerator(samples_gen))
-lookbackDat = lazyDat.withLookback(lookback)
-batch_gen = lookbackDat.batch(batch_size)
-
-tmp1=next(batch_gen)
-print(tmp1.getFeaturesAs("array"))
-print(tmp1.getLabelsAs("array"))
-tmp2=next(batch_gen)
-print(tmp2.getFeaturesAs("array"))
-print(tmp2.getLabelsAs("array"))
-tmp3=next(batch_gen)
-tmp4=next(batch_gen)
-tmp5=next(batch_gen)
-tmp6=next(batch_gen)
+if __name__ == "__main__":
+    unittest.main()
