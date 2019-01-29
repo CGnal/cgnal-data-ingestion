d20eb63bcce2774e29e9d449067d75e3a639a4db
NicolaDonelli <ndonelli@cgnal.com>
2019-01-29 11:00:17 +0100

    [MISC] Updated update_pkg.sh

--- data-model/src/main/python/update_pkg.sh
+++ data-model/src/main/python/update_pkg.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 python setup.py sdist
-source ~/venvs/cgnal-data-ingestion/bin/activate
-pip install -I --upgrade dist/cgnal-core-1.0.0.tar.gz
+pip install --upgrade $(ls -l1 dist/*.tar.gz | tail -n1)
