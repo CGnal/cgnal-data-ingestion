bacb9584816b80cf0fe0c7ab9cf0af00d6b79de9
NicolaDonelli <ndonelli@cgnal.com>
2021-08-25 18:08:50 +0200

    [MISC] Typing cgnal.tests

--- data-model/src/main/python/cgnal/tests/core.py
+++ data-model/src/main/python/cgnal/tests/core.py
@@ -1,28 +1,21 @@
-import pandas as pd
-import numpy as np
+import pandas as pd  # type: ignore
+import numpy as np  # type: ignore
 from time import time
+from typing import Callable
 from unittest import TestCase as CoreTestCase
 from cgnal.logging.defaults import WithLogging
 
 
-def logTest(test):
-    def wrap(obj):
-        t0 = time()
-        obj.logger.info(f"Executing Test {str(test.__name__)}")
-        test(obj)
-        obj.logger.info(f"Execution Time: {time() - t0} secs")
-    return wrap
-
-
 class TestCase(CoreTestCase, WithLogging):
 
-    def compareLists(self, first, second, strict=False):
+    def compareLists(self, first: list, second: list, strict: bool = False) -> None:
         if strict:
-            [self.assertEqual(item1, item2) for item1, item2 in zip(first, second)]
+            for item1, item2 in zip(first, second):
+                self.assertEqual(item1, item2)
         else:
             self.assertTrue(len(set(first).intersection(second)) == len(set(first)))
 
-    def compareDicts(self, first, second, strict=False):
+    def compareDicts(self, first: dict, second: dict, strict: bool = False) -> None:
         for key in set(first.keys()).union(second.keys()):
             if isinstance(first[key], dict):
                 self.compareDicts(first[key], second[key])
@@ -31,19 +24,19 @@ class TestCase(CoreTestCase, WithLogging):
             else:
                 self.assertEqual(first[key], second[key])
 
-    def compareDataFrames(self, first, second, msg):
+    def compareDataFrames(self, first: pd.DataFrame, second: pd.DataFrame, msg: str) -> None:
         try:
             pd.testing.assert_frame_equal(first, second)
         except AssertionError as e:
             raise self.failureException("Input series are different") from e
 
-    def compareSeries(self, first, second, msg):
+    def compareSeries(self, first: pd.Series, second: pd.Series, msg: str) -> None:
         try:
             pd.testing.assert_series_equal(first, second)
         except AssertionError as e:
             raise self.failureException("Input series are different") from e
 
-    def compareArrays(self, first, second, msg):
+    def compareArrays(self, first: np.ndarray, second: np.ndarray, msg: str) -> None:
         try:
             np.testing.assert_almost_equal(first, second, decimal=7)
         except AssertionError as e:
@@ -53,3 +46,12 @@ class TestCase(CoreTestCase, WithLogging):
         self.addTypeEqualityFunc(pd.DataFrame, self.compareDataFrames)
         self.addTypeEqualityFunc(pd.Series, self.compareSeries)
         self.addTypeEqualityFunc(np.ndarray, self.compareArrays)
+
+
+def logTest(test: Callable[[TestCase], None]) -> Callable[[TestCase], None]:
+    def wrap(obj):
+        t0 = time()
+        obj.logger.info(f"Executing Test {str(test.__name__)}")
+        test(obj)
+        obj.logger.info(f"Execution Time: {time() - t0} secs")
+    return wrap
